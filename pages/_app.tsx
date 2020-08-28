8001043256b6e4bc3101e1ada154cfa53d152755
ure-asuslaptop <urosr95@gmail.com>
2020-08-28 14:32:17 +0200

    added calendar & custom calendar styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import "../styles/globals.css";
+import "../styles/custom_datepicker.css";
 
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
